9aba7675789aa65fb9694e4bebfe151af74bc8d4
Bezibun <fauzanputa1526@gmail>
2024-05-10 08:36:42 +0700

    menghubungkan ke github dan start

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam adipisci
+        harum officia eius, modi amet! Consequuntur a, voluptatum at ad nobis
+        dicta veniam dolor corrupti, reiciendis doloremque ipsum aut quo.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
